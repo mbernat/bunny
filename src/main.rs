178edf970ae6d5252b13e5b050be23530c110874
Marek Bernat <mbernat@users.noreply.github.com>
2023-05-18 11:23:05 +0200

    Draw bunny

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::path::Path;
+use core::slice;
+use std::{fs::File, io::BufReader, mem::size_of, path::Path};
 
-use wgpu::{
-    Color, CommandEncoderDescriptor, FragmentState, PipelineLayoutDescriptor, PrimitiveState,
-    RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor,
-    ShaderModuleDescriptor, TextureViewDescriptor, VertexState, MultisampleState, ShaderSource, TextureFormat,
-};
+use wgpu::util::DeviceExt;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -42,8 +39,36 @@ struct Rendering {
     pipeline: wgpu::RenderPipeline,
 }
 
+fn cast_to_bytes<T>(slice: &[T]) -> &[u8] {
+    // SAFETY: input slice contains valid data and outlives the return value
+    unsafe { slice::from_raw_parts(slice.as_ptr() as *const u8, size_of::<T>() * slice.len()) }
+}
+
 impl Rendering {
     fn render(self: &Rendering, state: &GpuState) {
+        use wgpu::*;
+
+        let input = BufReader::new(File::open("bunny.obj").expect("bunny.obj not found"));
+        let bunny: obj::Obj<obj::Position> = obj::load_obj(input).expect("Not a valid .obj file");
+
+        let positions = &bunny.vertices;
+        let vertex_buffer = state
+            .device
+            .create_buffer_init(&util::BufferInitDescriptor {
+                label: Some("Main vertex buffer"),
+                contents: cast_to_bytes(positions),
+                usage: BufferUsages::VERTEX,
+            });
+
+        let indices = &bunny.indices[..];
+        let index_buffer = state
+            .device
+            .create_buffer_init(&util::BufferInitDescriptor {
+                label: Some("Main index buffer"),
+                contents: cast_to_bytes(indices),
+                usage: BufferUsages::INDEX,
+            });
+
         let mut encoder = state
             .device
             .create_command_encoder(&CommandEncoderDescriptor::default());
@@ -71,7 +96,9 @@ impl Rendering {
             });
 
             pass.set_pipeline(&self.pipeline);
-            pass.draw(0..3, 0..1);
+            pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+            pass.set_index_buffer(index_buffer.slice(..), IndexFormat::Uint16);
+            pass.draw_indexed(0..(indices.len() as u32), 0, 0..2);
         }
 
         let command_buffers = [encoder.finish()];
@@ -82,6 +109,7 @@ impl Rendering {
 
 async fn setup_wgpu(window: &Window) -> GpuState {
     use wgpu::*;
+
     let instance = Instance::new(InstanceDescriptor::default());
     let adapter = instance
         .request_adapter(&RequestAdapterOptions::default())
@@ -101,8 +129,8 @@ async fn setup_wgpu(window: &Window) -> GpuState {
         &SurfaceConfiguration {
             usage: TextureUsages::RENDER_ATTACHMENT,
             format: TextureFormat::Bgra8Unorm,
-            width: 80,
-            height: 60,
+            width: 800,
+            height: 600,
             present_mode: PresentMode::Fifo,
             alpha_mode: CompositeAlphaMode::Auto,
             view_formats: vec![],
@@ -117,19 +145,35 @@ async fn setup_wgpu(window: &Window) -> GpuState {
 }
 
 fn setup_pipeline(state: &GpuState) -> Rendering {
+    use wgpu::*;
+
+    /*
+    // For bind groups
     let pipeline_layout = state
         .device
-        .create_pipeline_layout(&PipelineLayoutDescriptor::default()); //&PipelineLayoutDescriptor { label: (), bind_group_layouts: (), push_constant_ranges: () });
+        .create_pipeline_layout(&PipelineLayoutDescriptor::default());
+    */
 
     let source = ShaderSource::Wgsl(include_str!("main.wgsl").into());
     let module = state.device.create_shader_module(ShaderModuleDescriptor {
         label: Some("Main shader module"),
         source,
     });
+
+    let vertex_layout = VertexBufferLayout {
+        array_stride: 12,
+        step_mode: VertexStepMode::Vertex,
+        attributes: &[VertexAttribute {
+            format: VertexFormat::Float32x3,
+            offset: 0,
+            shader_location: 0,
+        }],
+    };
+
     let vertex = VertexState {
         module: &module,
         entry_point: "vertex",
-        buffers: &[],
+        buffers: &[vertex_layout],
     };
     let fragment = FragmentState {
         module: &module,
@@ -137,11 +181,11 @@ fn setup_pipeline(state: &GpuState) -> Rendering {
         targets: &[Some(TextureFormat::Bgra8Unorm.into())],
     };
 
-    let primitive= PrimitiveState {
+    let primitive = PrimitiveState {
         topology: wgpu::PrimitiveTopology::TriangleList,
         strip_index_format: None,
         front_face: wgpu::FrontFace::Ccw,
-        cull_mode: None,
+        cull_mode: Some(Face::Back),
         unclipped_depth: false,
         polygon_mode: wgpu::PolygonMode::Fill, // Line
         conservative: false,
@@ -152,7 +196,7 @@ fn setup_pipeline(state: &GpuState) -> Rendering {
         .create_render_pipeline(&RenderPipelineDescriptor {
             label: Some("Main render pipeline"),
             layout: None, // Some(&pipeline_layout);
-            vertex: vertex,
+            vertex,
             primitive,
             depth_stencil: None,
             multisample: MultisampleState::default(),
--- src/main.wgsl
+++ src/main.wgsl
@@ -1,14 +1,32 @@
+struct VertexInput {
+    @location(0) position: vec3<f32>
+}
+
 struct VertexOutput {
+    // 3d space? Values between -1 and 1
     @builtin(position) position: vec4<f32>,
-};
+    @location(0) color: vec4<f32>
+}
+
+struct FragmentInput {
+    // Screen space. Values between (0, 0) and (width, height)
+    @builtin(position) position: vec4<f32>,
+    @location(0) color: vec4<f32>
+}
 
 @vertex
-fn vertex(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let id = f32(in_vertex_index);
-    return vec4(0.5 * (id - 1.0), 0.5 * (1.0 - abs(id - 1.0)), 0.0, 1.0);
+fn vertex(@builtin(vertex_index) index: u32, in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.position = vec4(in.position * 20.0, 1.0) + vec4(0.5, -2.0, 0.0, 1.0);
+    let id = f32(index) / 1000.0;
+    let r = i32(index) % 3 - 1;
+    let g = 1 - i32(index) % 3;
+    let b = 1 - abs(i32(index) % 3 - 1);
+    out.color = id * vec4(f32(r), f32(g), f32(b), 1.0);
+    return out;
 }
 
 @fragment
-fn fragment(@builtin(position) input: vec4<f32>) -> @location(0) vec4<f32> {
-    return vec4(1.0);
+fn fragment(in: FragmentInput) -> @location(0) vec4<f32> {
+    return in.color;
 }
